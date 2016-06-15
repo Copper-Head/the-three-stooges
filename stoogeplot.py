4c11186937db892690a94a4e3646c757f4b10abb
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-06-16 01:47:22 +0200

    Adds hinton plotting function in a new module.

---
+++ stoogeplot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def hinton_diagram(matrix, max_weight=None, ax=None):
+    """Draw Hinton diagram for visualizing a weight matrix.
+
+    Note that depending on the size of your matrix you might have to adjust
+    the size of your figure in order to display the squares right.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    if not max_weight:
+        max_weight = 2 ** np.ceil(np.log(np.abs(matrix).max()) / np.log(2))
+
+    ax.patch.set_facecolor('gray')
+    ax.set_aspect('equal', 'box')
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+
+    for (x, y), w in np.ndenumerate(matrix):
+        color = 'white' if w > 0 else 'black'
+        size = np.sqrt(np.abs(w))
+        half = size / 2
+        rect = plt.Rectangle([x - half, y - half], size, size,
+                             facecolor=color, edgecolor=color)
+        ax.add_patch(rect)
+
+    # Not quite sure what these do atm
+    ax.autoscale_view()
+    ax.invert_yaxis()
