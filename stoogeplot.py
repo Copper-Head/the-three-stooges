5877c83472195cd5d55c99136e5cb69c425f0cf2
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-06-19 21:40:51 +0200

    Cleaned up hinton_diagram code

--- stoogeplot.py
+++ stoogeplot.py
@@ -11,15 +11,16 @@ def hinton_diagram(matrix, max_weight=None, ax=None, xticks=None):
     if ax is None:
         ax = plt.gca()
 
-    # max_weight = np.abs(matrix).max()
     if not max_weight:
         max_weight = 2 ** np.ceil(np.log(np.abs(matrix).max()) / np.log(2))
 
     ax.patch.set_facecolor('gray')
     ax.set_aspect('equal', 'box')
-    # ax.xaxis.set_major_locator(plt.NullLocator())
+    # Not interested in yaxis ticks
     ax.yaxis.set_major_locator(plt.NullLocator())
+
     if xticks:
+        # Don't want the ticks visible, but labels should be legible
         ax.xaxis.set_tick_params(width=0, labelsize=30)
         ax.set_xticks(range(len(xticks)))
         ax.set_xticklabels(list(xticks))
@@ -28,7 +29,6 @@ def hinton_diagram(matrix, max_weight=None, ax=None, xticks=None):
         color = 'white' if w > 0 else 'black'
         size = np.sqrt(np.abs(w) / max_weight)
         half = size / 2
-        # half = 0.5
         rect = plt.Rectangle([x - half, y - half], size, size,
                              facecolor=color, edgecolor=color)
         ax.add_patch(rect)
