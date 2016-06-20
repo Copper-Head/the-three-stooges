73effc6d5a127e62f1ce0f02d338b653ae987512
Martin Klotz <MartinKl@hu-berlin.de>
2016-06-20 20:09:04 +0200

    introduced a marker

--- train_tbptt.py
+++ train_tbptt.py
@@ -83,6 +83,7 @@ state_to_compare = list(filter(lambda x: x.name == 'sequencegenerator_cost_matri
 def modifier_function(iterations_done):
     aggr = AggregationBuffer(state_to_compare, use_take_last=True)
     values = aggr.get_aggregated_values()
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', values[state_to_compare.name])
     return values[state_to_compare.name][0][-1][0]
 
