1a33f3773d7bac9aade76cccacb1dbb49c508310
Martin Klotz <MartinKl@hu-berlin.de>
2016-06-22 21:46:46 +0200

    typo

--- train_tbptt.py
+++ train_tbptt.py
@@ -96,7 +96,7 @@ def modifier_function(iterations_done, old_value):
     new_value = values[state_to_compare.name]
     aggr.initialize_aggregators()  # TODO what's the purpose of that? I observed them do it in the monitoring extensions after every request
     #new_value = ones(10, dtype='float32')
-    print('OLD ..:'. old_value)
+    print('OLD ..:', old_value)
     print('NEW in: ', new_value)
     return new_value[-1][0]
 
