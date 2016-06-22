aef68b6bd6bb8697bc38ec3f8ff2dc5b656623c9
Martin Klotz <MartinKl@hu-berlin.de>
2016-06-22 21:31:37 +0200

    just another print out, after that I try a theano function, maybe

--- train_tbptt.py
+++ train_tbptt.py
@@ -93,12 +93,12 @@ aggr.initialize_aggregators()
 
 def modifier_function(iterations_done, old_value):
     values = aggr.get_aggregated_values()
-    new_value = values[state_to_compare.name][0][-1]
+    new_value = values[state_to_compare.name]
     aggr.initialize_aggregators()  # TODO what's the purpose of that? I observed them do it in the monitoring extensions after every request
     #new_value = ones(10, dtype='float32')
     print(old_value)
     print(new_value)
-    return new_value
+    return new_value[0][-1]
 
 init_state_modifier = SharedVariableModifier(network.transitions[-1].initial_state_, function=modifier_function, after_batch=True)
 
