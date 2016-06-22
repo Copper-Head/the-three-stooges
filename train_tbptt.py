3c89f9d3b58580ffa71da4110e1e99e1b01e570b
Martin Klotz <MartinKl@hu-berlin.de>
2016-06-22 22:03:38 +0200

    data seems to be accumulated in reversed order, so I took the first entry of the state vector (not the last) as new initial state

--- train_tbptt.py
+++ train_tbptt.py
@@ -92,13 +92,19 @@ aggr = AggregationBuffer(variables=[state_to_compare], use_take_last=True)
 aggr.initialize_aggregators()
 
 def modifier_function(iterations_done, old_value):
+    """
+    Note about this method: The accumulator seems to get the state vector in reversed order
+    :param iterations_done:
+    :param old_value:
+    :return:
+    """
     values = aggr.get_aggregated_values()
     new_value = values[state_to_compare.name]
     aggr.initialize_aggregators()  # TODO what's the purpose of that? I observed them do it in the monitoring extensions after every request
     #new_value = ones(10, dtype='float32')
-    print('OLD ..:', old_value)
-    print('NEW in: ', new_value[-1][0], new_value[0][0], sep='\n')
-    return new_value[-1][0]
+    #print('OLD ..:', old_value)
+    #print('NEW in: ', new_value[-1][0], new_value[0][0], sep='\n')
+    return new_value[0][0]
 
 init_state_modifier = SharedVariableModifier(network.transitions[-1].initial_state_, function=modifier_function, after_batch=True)
 
