e8beb1cc4da5fcc835e54316a698a21be112a3ab
Jens Johannsmeier <Jens Johannsmeier>
2016-06-23 16:29:03 +0200

    testing manual access to data stream

--- jens_playground.py
+++ jens_playground.py
@@ -1,8 +1,12 @@
 import cPickle
 
+from fuel.datasets.hdf5 import H5PYDataset
+from fuel.streams import DataStream
+from fuel.schemes import SequentialScheme
 import numpy
 from scipy.stats import pearsonr
 
+from custom_blocks import PadAndAddMasks
 from network import NetworkType, Network
 from util import StateComputer, mark_seq_len
 
@@ -12,6 +16,18 @@ lstm_net.set_parameters('seqgen_lstm_512_512_512.pkl')
 map_chr_2_ind = cPickle.load(open("char_to_ind.pkl"))
 
 sc = StateComputer(lstm_net.cost_model, map_chr_2_ind)
+
+
+train_data = H5PYDataset("bible.hdf5", which_sets=("train",), load_in_memory=True)
+data_stream = PadAndAddMasks(
+    DataStream.default_stream(dataset=train_data, iteration_scheme=SequentialScheme(train_data.num_examples,
+                                                                                    batch_size=128)),
+    produces_examples=False)
+iterator = data_stream.get_epoch_iterator()
+while iterator:
+    something = next(iterator)
+    print type(something)
+raw_input("GOT THROUGH")
 
 verse = "1:7 And God made the firmament, and divided the waters which were " \
         "under the firmament from the waters which were above the firmament: " \
