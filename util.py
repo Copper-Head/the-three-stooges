17dbd06e1a2a66abb19bc11c0ef24e4bd1401a75
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-06-24 01:37:57 +0200

    Adds func for computing mean given variable name and position.

--- util.py
+++ util.py
@@ -96,3 +96,15 @@ def mark_word_boundaries(seq):
         "\n"
     }
     return numpy.array([1 if char in wb else 0 for char in seq])
+
+
+def var_pos_mean(value_list, var_name, pos_indx=None):
+    """Retrieves and averages variable's value given it's name and some index.
+
+    value_list is assumed to be a list of StateComputer.read_single_sequence results.
+    pos_indx can in theory be a range of indices.
+    """
+    var_pos_vals = [v[var_name] for v in value_list]
+    if pos_indx:
+        var_pos_vals = [v[pos_indx] for v in var_pos_vals]
+    return numpy.mean(var_pos_vals, axis=0)
