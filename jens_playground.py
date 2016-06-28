a2a4aeda2c733e951404dd33331d4bf52a38561f
Jens Johannsmeier <Jens Johannsmeier>
2016-06-28 11:29:06 +0200

    trying to have a look at prediction probabilities

--- jens_playground.py
+++ jens_playground.py
@@ -1,16 +1,19 @@
 import cPickle
 
+import numpy
+from blocks.filter import VariableFilter
 from fuel.datasets.hdf5 import H5PYDataset
 from fuel.streams import DataStream
 from fuel.schemes import SequentialScheme
-import numpy
 from scipy.stats import pearsonr
+from theano import function
 
 from custom_blocks import PadAndAddMasks
 from network import NetworkType, Network
 from util import StateComputer, mark_seq_len, mark_word_boundaries
 
 
+# TODO get these into util.py, maybe in a prettier form
 def mark_seq_len_batch(seq_batch, mask_batch):
     # get markers separately, then reshape
     padded_markers = numpy.array([numpy.arange(len(seq)) for seq in seq_batch])
@@ -30,11 +33,18 @@ lstm_net.set_parameters('seqgen_lstm_512_512_512.pkl')
 map_chr_2_ind = cPickle.load(open("char_to_ind.pkl"))
 map_ind_2_chr = cPickle.load(open("ind_to_char.pkl"))
 
+readouts = VariableFilter(theano_name="readout_readout_output_0")(lstm_net.cost_model.variables)
+char_probs = lstm_net.generator.readout.emitter.probs(readouts)
+
+prob_function = function([lstm_net.x], char_probs)
+
+
+
+"""
 sc = StateComputer(lstm_net.cost_model, map_chr_2_ind)
 correlation_dict = dict()
 for name in sc.state_var_names:
     correlation_dict[name] = numpy.zeros(512, dtype=float)  # TODO NOT VERY GENERAL!!!
-
 
 valid_data = H5PYDataset("bible.hdf5", which_sets=("valid",), load_in_memory=True)
 data_stream = PadAndAddMasks(
@@ -42,39 +52,10 @@ data_stream = PadAndAddMasks(
                                                                                     batch_size=128)),
     produces_examples=False)
 iterator = data_stream.get_epoch_iterator()
-"""
-try:
-    while iterator:
-        seq_batch, mask_batch = next(iterator)
-        state_batch_dict = sc.read_sequence_batch(seq_batch, mask_batch)
-        # unfortunately we don't have a "batched correlation", and padding could lead to problems here, so one by one...
-        # first iterate over the different layers and states/cells
-        for state_type in state_batch_dict:
-            state_batch = state_batch_dict[state_type]
-            # and then over the different sequences in the batch (always remember, axis 1, not 0)
-            for sequence_ind in xrange(state_batch.shape[1]):
-                state_seq = state_batch[:, sequence_ind, :]
-                mask = mask_batch[sequence_ind, :]  # mask is NOT transposed!!
-                state_seq = state_seq[mask == 1, :]  # throw away padding
-                # now get a marker and compute separately the correlation of each state seq with the marker seq
-                seq_len_correlator = mark_word_boundaries([map_ind_2_chr[ind] for ind in seq_batch[sequence_ind, mask == 1]])
-                for dim in xrange(state_seq.shape[1]):
-                    correlation_dict[state_type][dim] += pearsonr(state_seq[:, dim], seq_len_correlator)[0]
-        print "MADE IT THROUGH BATCH"
-except StopIteration:
-    pass
-# at the very end, we need to divide all our summed up correlations by the number of sequences to get the average
-for state_name in correlation_dict:
-    correlation_dict[state_name] /= valid_data.num_examples
-    print state_name
-    print correlation_dict[state_name]
-    print "LARGEST:", max(correlation_dict[state_name]), min(correlation_dict[state_name])
-    print "\n\n"
-"""
 
 state_super_dict = dict()
 for name in sc.state_var_names:
-    state_super_dict[name] = numpy.empty(shape=(0, 512))  # TODO NUMBER OF CELLS NOT VERY GENERAL
+    state_super_dict[name] = numpy.empty(shape=(0, 512))  # TODO NOT VERY GENERAL AGAIN
 super_marker = numpy.empty(shape=(0,))
 
 prediction_alignment = True
@@ -115,3 +96,4 @@ for state_name in correlation_dict:
     print correlation_dict[state_name]
     print "LARGEST:", max(correlation_dict[state_name]), min(correlation_dict[state_name])
     print "\n\n"
+"""
